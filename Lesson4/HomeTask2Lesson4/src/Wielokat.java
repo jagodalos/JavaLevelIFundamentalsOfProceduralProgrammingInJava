dadc2cebb527b9891499c7ce13331666e174ade4
jagod <jagod@DESKTOP-73NPDN9>
2016-12-17 17:47:34 +0100

    HomeTask2Lesson4

--- Lesson4/HomeTask2Lesson4/src/Wielokat.java
+++ Lesson4/HomeTask2Lesson4/src/Wielokat.java
@@ -5,20 +5,25 @@ public class Wielokat {
 
 	public static void main(String[] args) {
 
-		int iloscKatow = 4;
+		System.out.println("PROGRAM LICZACY OBWOD WIELOKATA.");
+		System.out.println();
+
+		int numberOfAngles = 3;
 		int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
 
-		if (iloscKatow < 3) {
+		if (numberOfAngles < 3) {
 
 			System.out.println("Ilosc katow w wielokacie jest nieprawidlowa.");
 			System.out.println("Zdefiniuj ilosc katow jeszcze raz");
 
 		}
+		System.out.println("Liczba katow w Twoim wielokacie to: " + numberOfAngles);
 		System.out.println();
 
 		Scanner scInt = new Scanner(System.in);
-		if (iloscKatow == 3) {
-			for (int i = 2; i < iloscKatow; i++) {
+
+		if (numberOfAngles == 3) {
+			for (int i = 2; i < numberOfAngles; i++) {
 				System.out.println("Podaj bok a");
 				a = scInt.nextInt();
 				System.out.println("Podaj bok b");
@@ -26,9 +31,10 @@ public class Wielokat {
 				System.out.println("Podaj bok c");
 				c = scInt.nextInt();
 			}
-			System.out.println("obwod" + (a + b + c));
-		} else if (iloscKatow == 4) {
-			for (int i = 3; i < iloscKatow; i++) {
+			System.out.println("Obwod wielokata : " + (a + b + c));
+
+		} else if (numberOfAngles == 4) {
+			for (int i = 3; i < numberOfAngles; i++) {
 				System.out.println("Podaj bok a");
 				a = scInt.nextInt();
 				System.out.println("Podaj bok b");
@@ -38,9 +44,10 @@ public class Wielokat {
 				System.out.println("Podaj bok d");
 				d = scInt.nextInt();
 			}
-			System.out.println("obwod" + (a + b + c + d));
-		} else if (iloscKatow == 5) {
-			for (int i = 4; i < iloscKatow; i++) {
+			System.out.println("Obwod wielokata : " + (a + b + c + d));
+
+		} else if (numberOfAngles == 5) {
+			for (int i = 4; i < numberOfAngles; i++) {
 				System.out.println("Podaj bok a");
 				a = scInt.nextInt();
 				System.out.println("Podaj bok b");
@@ -52,9 +59,10 @@ public class Wielokat {
 				System.out.println("Podaj bok e");
 				e = scInt.nextInt();
 			}
-			System.out.println("obwod" + (a + b + c + d + e));
-		} else if (iloscKatow == 6) {
-			for (int i = 5; i < iloscKatow; i++) {
+			System.out.println("Obwod wielokata : " + (a + b + c + d + e));
+
+		} else if (numberOfAngles == 6) {
+			for (int i = 5; i < numberOfAngles; i++) {
 				System.out.println("Podaj bok a");
 				a = scInt.nextInt();
 				System.out.println("Podaj bok b");
@@ -68,38 +76,28 @@ public class Wielokat {
 				System.out.println("Podaj bok f");
 				f = scInt.nextInt();
 			}
-			System.out.println("obwod" + (a + b + c + d + f));
+			System.out.println("Obwod wielokata : " + (a + b + c + d + f));
 		}
 
 		else {
 			Random generator = new Random();
-			System.out.println("The ten random values are: ");
-			int[] table = new int[iloscKatow];
-			for(int i = 0; i < iloscKatow; i++) {
-			   int wynik = generator.nextInt(13);
-			   table[i] = wynik;
-			   System.out.print(" ," + wynik);
+			System.out.println("Wylosowane liczby: ");
+			int[] table = new int[numberOfAngles];
+			for (int i = 0; i < numberOfAngles; i++) {
+				int result = generator.nextInt(13);
+				table[i] = result;
+				System.out.print(" " + result);
 			}
-		
+
 			int sum = 0;
-			for ( int i : table){
-				sum=sum+i;
-				
+			for (int i : table) {
+				sum = sum + i;
+
 			}
-			System.out.println("obwod" + sum);
-			
-			
-			}
-			
-			
+			System.out.println();
+			System.out.println("Obwod wielokata : " + sum);
 
 		}
+
 	}
-
-
-
-
-
-
-
-
+}
